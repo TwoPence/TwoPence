3e1408243b9d39c67f17de92c9eaeacd4300dd81
wfgilman <wfgilman@gmail.com>
2017-06-06 23:16:19 -0700

    Completed onboarding. (#28)

--- TwoPence/AppCopy.swift
+++ TwoPence/AppCopy.swift
@@ -13,10 +13,14 @@ struct AppCopy {
     static let linkFirstSponsor = "At TwoPence we are proud of all you’ve accomplished in higher education, and we are betting others are proud too! \n\n Linking a Sponsor allows someone to recognize all the hard work you’ve been putting into saving for Financial Freedom-and lend a helping hand."
     static let firstJolt = "Jolt makes a one-time withdrawal from your account and pays it toward your debt. Think of Jolt as the icing on the cake of your Financial Freedom plan. Use it when you’ve been spending less than usual, came into some extra cash, or just plain want to grab some debt-reduction-goodness at the end of the day."
     
-    static let onboardingScreen1 = "Swipe to learn more"
-    static let onboardingScreen2 = "TwoPence makes savings simple. \n\n TwoPence generates savings by adding an extra 10% to every dollar you spend."
-    static let onboardingScreen3 = "TwoPence makes accountability easy. \n\n These savings are automatically withdrawn from your account and sent as additional payments to your student loan."
-    static let onboardingScreen4 = "TwoPence makes the future attainable. \n\n Once debt-free, your savings are routed to a personalized investment portfolio."
+    static let onboardingCall0 = "Swipe to learn more"
+    static let onboardingCall1 = "TwoPence makes savings simple."
+    static let onboardingCall2 = "TwoPence makes accountability easy."
+    static let onboardingCall3 = "TwoPence makes the future attainable."
+    
+    static let onboardingResponse1 = "TwoPence generates savings by adding an extra 10% to every dollar you spend."
+    static let onboardingResponse2 = "These savings are automatically withdrawn from your account and sent as additional payments to your student loan."
+    static let onboardingResponse3 = "Once debt-free, your savings are routed to a personalized investment portfolio."
     
     static let linkSponsorMessage = "Enter a personalized message here."
     
--- TwoPence/IntroViewController.swift
+++ TwoPence/IntroViewController.swift
@@ -18,8 +18,8 @@ class IntroViewController: UIViewController {
     @IBOutlet weak var signinButton: TKTransitionSubmitButton!
     
     var logoView: LogoView?
-    var animationView: LOTAnimationView?
-    let dataSourceArray = [AppCopy.onboardingScreen1, AppCopy.onboardingScreen2, AppCopy.onboardingScreen3, AppCopy.onboardingScreen4]
+    let callArray = ["", AppCopy.onboardingCall1, AppCopy.onboardingCall2, AppCopy.onboardingCall3]
+    let responseArray = ["Swipe to learn more", AppCopy.onboardingResponse1, AppCopy.onboardingResponse2, AppCopy.onboardingResponse3]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,15 +41,6 @@ class IntroViewController: UIViewController {
         animateLogo()
     }
     
-    func setAnimationView() {
-        animationView = LOTAnimationView(name: "Watermelon")
-        guard let animationView = self.animationView else { return }
-        animationView.frame = CGRect(x: 0, y: 20, width: self.view.frame.size.width, height: 400)
-        animationView.contentMode = .scaleAspectFill
-        animationView.isUserInteractionEnabled = false
-        view.addSubview(animationView)
-    }
-    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         UIApplication.shared.statusBarStyle = .default
@@ -69,33 +60,75 @@ class IntroViewController: UIViewController {
     }
     
     func setUpScrollView() {
+        let pages: Int = 4
+        let pageWidth = self.view.bounds.width
+        let pageHeight = scrollView.bounds.height
+        
         scrollView.delegate = self
         scrollView.isPagingEnabled = true
         scrollView.bounces = false
         scrollView.showsHorizontalScrollIndicator = false
-        scrollView.contentSize = CGSize(width: self.view.frame.width * 6, height: 0)
+        scrollView.contentSize = CGSize(width: pageWidth * CGFloat(pages), height: pageHeight)
         
-        for i in 0...dataSourceArray.count - 1 {
-            setAnimationView()
-            
-            let labelWidth: CGFloat = 300
-            let positionX = ((self.scrollView.frame.width - labelWidth) / 2) + CGFloat(i) * self.scrollView.frame.width
-            let positionY = self.scrollView.frame.height - 30
-            let label = UILabel(frame: CGRect(x: positionX, y: positionY, width: labelWidth, height: 30))
-            label.textAlignment = .center
-            label.font = UIFont(name: AppFontName.regular, size: 17)
-            label.textColor = .black
-            label.text = dataSourceArray[i]
-            
-            if i == 0 {
-                logoView = LogoView(frame: CGRect(x: positionX, y: 0, width:237, height: 224))
-                logoView?.center = scrollView.center
-                scrollView.addSubview(logoView!)
-            }
-            
-            label.isUserInteractionEnabled = false
+        // Ending Logo.
+        let positionX = 3.5 * self.view.bounds.width
+        let positionY = scrollView.bounds.height / 2
+        let logoImageView = UIImageView(image: #imageLiteral(resourceName: "logo"))
+        logoImageView.center = CGPoint(x: positionX, y: positionY)
+        logoImageView.alpha = 1.0
+        logoImageView.tag = 4
+        scrollView.addSubview(logoImageView)
 
-            scrollView.addSubview(label)
+        for page in 0..<pages {
+            // Text.
+            let textHeight: CGFloat = 80.0
+            let textInset: CGFloat = 16.0
+            let font = UIFont(name: AppFontName.bold, size: 17)
+            
+            // Call Label.
+            let callLabelHeight: CGFloat = 20
+            let callLabelFrame = CGRect(x: CGFloat(page) * pageWidth + textInset, y: pageHeight - textHeight, width: (pageWidth - textInset * 2), height: callLabelHeight)
+            let callLabel = UILabel(frame: callLabelFrame)
+            callLabel.textAlignment = .center
+            callLabel.font = font
+            callLabel.textColor = AppColor.DarkSeaGreen.color
+            callLabel.numberOfLines = 1
+            callLabel.text = callArray[page]
+            scrollView.addSubview(callLabel)
+            
+            // Response Label.
+            let responseFrame = CGRect(x: CGFloat(page) * pageWidth + textInset, y: pageHeight - textHeight + callLabelHeight, width: (pageWidth - textInset * 2), height: textHeight - callLabelHeight)
+            let responseLabel = UILabel(frame: responseFrame)
+            responseLabel.textAlignment = .center
+            responseLabel.font = page == 0 ? font : font?.withSize(15)
+            responseLabel.textColor = AppColor.Charcoal.color
+            responseLabel.numberOfLines = 3
+            responseLabel.text = responseArray[page]
+            scrollView.addSubview(responseLabel)
+            
+            if page == 0 {
+                // Logo.
+                let logoFrame = CGRect(x: 0, y: 0, width: 237, height: 224)
+                logoView = LogoView(frame: logoFrame)
+                logoView?.center = scrollView.center
+                if let _logoView = logoView {
+                    scrollView.addSubview(_logoView)
+                }
+            } else {
+                // Animation.
+                let animationHeight = pageHeight - textHeight
+                let animationFrame = CGRect(x: CGFloat(page) * pageWidth, y: 0, width: pageWidth, height: animationHeight)
+                let animation = "segment\(page)"
+                print("\(animation)")
+                if let animationView = LOTAnimationView(name: animation) {
+                    animationView.frame = animationFrame
+                    animationView.contentMode = .scaleAspectFill
+                    animationView.clipsToBounds = true
+                    animationView.animationSpeed = page == 3 ? 1.5 : 1.0
+                    animationView.tag = page
+                    scrollView.addSubview(animationView)
+                }
+            }
         }
     }
     
@@ -126,16 +159,23 @@ class IntroViewController: UIViewController {
 }
 
 extension IntroViewController: UIScrollViewDelegate {
-    
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        
-        let progress = scrollView.contentOffset.x / scrollView.contentSize.width
-        animationView?.animationProgress = progress
-    }
-    
-    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        let pageNumber = round(scrollView.contentOffset.x / scrollView.frame.size.width)
-        pageControl.currentPage = Int(pageNumber)
+
+   func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let logoView = scrollView.viewWithTag(4) as! UIImageView
+        logoView.alpha = 0.0
+        scrollView.bringSubview(toFront: logoView)
+        let pageNumber = Int(scrollView.contentOffset.x / scrollView.frame.size.width)
+        pageControl.currentPage = pageNumber
+        if pageNumber != 0 {
+            let animationView = scrollView.viewWithTag(pageNumber) as! LOTAnimationView
+            animationView.play(completion: { (_) in
+                if pageNumber == 3 {
+                    UIView.animate(withDuration: 0.5, animations: {
+                        logoView.alpha = 1.0
+                    })
+                }
+            })
+        }
     }
     
     func showLogin(){
